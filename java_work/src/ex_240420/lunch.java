11a6c9bded19af151bfe308b974abb9c12800ab0
ye0nsuuuuu <1eey3onsu@gmail.com>
2024-05-01 21:26:31 +0900

    first commit

--- java_work/src/ex_240420/lunch.java
+++ java_work/src/ex_240420/lunch.java
@@ -4,7 +4,8 @@ public class lunch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("오늘 점심은 서브웨이");
+		System.out.println("오늘 점심은 서브웨이");
+
 	}
 
 }
